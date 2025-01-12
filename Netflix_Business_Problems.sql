7540c5514febb0625135385f7689fae2a76f5382
Nithin Samuel John <65074441+nithin0310@users.noreply.github.com>
2025-01-13 00:05:17 +0530

    Add files via upload

---
+++ Netflix_Business_Problems.sql
@@ -0,0 +1,22 @@
+-- 15 Business Problems & Solutions
+
+1.Determine the distribution of content types by counting the number of Movies and TV Shows on Netflix.
+2.Identify the most frequent rating for Movies and TV Shows.
+3.Retrieve a list of all Movies released in a specified year (e.g., 2020).
+4.Rank the top 5 countries with the highest number of content items available on Netflix.
+5.Find the Movie with the longest runtime.
+6.Analyze the content added to Netflix over the last 5 years.
+7.Retrieve all Movies and TV Shows directed by 'Rajiv Chilaka'.
+8.List all TV Shows that have more than 5 seasons.
+9.Count the number of content items available in each genre.
+10.Calculate the average yearly content released in India on Netflix and identify the top 5 years with the highest averages.
+11.Retrieve a list of all Movies categorized as documentaries.
+12.Identify all content items that do not have a director assigned.
+13.Count how many Movies actor 'Salman Khan' has appeared in over the last 10 years.
+14.Identify the top 10 actors who have appeared in the highest number of Movies produced in India.
+15.Categorize content based on the presence of the keywords 'kill' and 'violence' in their descriptions. Label content containing these keywords as 'Bad' and all other content as 'Good,' and count the number of items in each category.
+
+
+
+
+
